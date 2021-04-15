ec163ac70ceae1975809943635d6740a07364034
codingneverends <gaganlal7171@gmail.com>
2021-04-15 17:29:09 +0530

    messaging

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import WaitandQueue from './WaitandQueue'
 import Game from './Game'
 import GameOver from './GameOver'
 import IntrotoGame from './IntrotoGame'
+import chatlogo from './logos/chat.png'
+import closeimg from './logos/close.png'
+import sendimg from './logos/sendimg.png'
 
 import { useAuthState } from 'react-firebase-hooks/auth'
 import useJSON from './useJSON';
@@ -87,8 +90,148 @@ function Topbar({user}){
     <div className="topbar">
       <img src={user.photoURL} alt="Can't be rendered"/>
       <h4>{user.displayName}</h4>
+      <Chat user={user}  room="" _public={true}/>
       <SignOut/>
     </div>
+  )
+}
+function Chat({room,user,_public}){
+  const [height,setheight]=useState("0px");
+  const [message,setmessage]=useState("");
+  const [senderr,setsenderr]=useState(" ");
+  const ref=database.ref(_public?"msgs":"Rooms/"+room+"/msgs");
+  const [msgs] = useJSON(ref);
+  const [highlight,sethighlight]=useState(false);
+  const [len,setlen]=useState(0);
+  const [unread,setunread]=useState(0);
+  const [lastlen,setlastlen]=useState(0);
+  useEffect(()=>{
+    if(height==="0px"){
+      sethighlight(true);
+      console.log("highlight");
+    }
+    // eslint-disable-next-line
+  },[len])
+  useEffect(()=>{
+    if(len!==msgs.length){
+      const val=(msgs.length-lastlen>0)?msgs.length-lastlen:0;
+      setunread(val);
+      setlen(msgs.length);
+      if(unread<0)
+        setunread(0);
+    } 
+    if(height!=="0px"){
+      sethighlight(false);
+      setunread(0);
+      setlastlen(len);
+    }
+    var msgsdiv=document.getElementById("chatsdiv");
+    msgsdiv.scrollTop=msgsdiv.scrollHeight;
+      // eslint-disable-next-line
+  },[msgs,height,len])
+  function checkmsg(e){
+    const msg=e.target.value;
+    setmessage(msg);
+    if(msg.length!==0)
+      setsenderr("");
+  }
+  function send(){
+    if(message.length===0){
+      setsenderr("Can't send a empty message.")
+      return;
+    }
+    setsenderr("")
+    var path="msgs";
+    if(!_public)
+      path="Rooms/"+room+"/msgs";
+    console.log(message);
+    database.ref(path).push({
+      uid : user.uid,
+      msg : message,
+      time : new Date().getTime(),
+      name : user.displayName,
+      img : user.photoURL
+    });
+    document.getElementById("sendmsg").value="";
+  }
+  function processtime(_time){
+    const diff=(new Date().getTime()-Number(_time))/1000;
+    const mins=Math.floor(diff/60);
+    if(mins===0)
+      return "just now";
+    if(mins===1)
+        return mins+" minute ago";
+    if(mins<60)
+      return mins+" minutes ago";
+    const hrs=Math.floor(diff/(60*60));
+    if(hrs===1)
+      return hrs+" hour ago";
+    if(hrs<24)
+      return hrs+" hours ago";
+    const days=Math.floor(diff/(60*60*24));
+    if(days===1)
+      return "yesterday";
+    if(days<7)
+      return days+" days ago";
+    const weeks=Math.floor(diff/(60*60*24*7));
+    if(weeks===1)
+      return weeks+" week ago";
+    if(weeks<5)
+      return weeks+" weeks ago";
+    const months=Math.floor(diff/(60*60*24*7*30));
+    if(months===1)
+      return months+" month ago";
+    if(months<12)
+      return months+" months ago";
+    const years=Math.floor(diff/(60*60*24*7*365));
+    if(years===1)
+      return years+" year ago";
+    return years+" years ago";
+    
+  }
+  return(
+    <>
+      <img src={chatlogo} style={{height:"50px",width:"50px",position:"fixed",zIndex:5,bottom:"40px",right:"40px",cursor:"pointer"}} onClick={()=>{setheight("100vh")}} alt="error loading.."/>
+      <div style={{position:"fixed",width:"100%",bottom:"0px",height:height,background:"#000000af",zIndex:7}} onClick={()=>{setheight("0px")}}></div>
+      <div style={{background:"#0f0",color:"#000",fontWeight:"bold",width:"30px",height:"30px",display:"flex",alignItems:"center",justifyContent:"center",borderRadius:"50%",bottom:"70px",right:"70px",position:"fixed",zIndex:6,cursor:"pointer"}} onClick={()=>{setheight("100vh")}}>{unread}</div>
+      <div style={{position:"fixed",bottom:"0px",width:"100%",maxWidth:"500px",background:"#000",overflow:"hidden",maxHeight:height,transition:"all 0.5s",zIndex:8,left:"50%",transform:"translateX(-50%)"}}>
+          <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+            <img src={chatlogo} style={{height:"50px",width:"50px",marginLeft:"20px"}} alt="error loading.."/>
+            <h2 style={{flexGrow:1,padding:"20px"}}>{_public?"Global Chat":"Messages"}</h2>
+            <img src={closeimg} style={{height:"50px",width:"50px",marginRight:"20px",cursor:"pointer"}} onClick={()=>{setheight("0px")}} alt="error loading.."/>
+          </div>
+          {
+            console.log(highlight)
+          }
+          <div style={{marginBottom:"10px",borderTop:"2px solid #ffffff8f",height:"60vh",overflowY:"scroll"}} id="chatsdiv">
+            {
+              msgs.map( _msg =>{
+                return ( 
+                  <div style={{padding:"5px",margin:"0px 10px",borderBottom:"2px solid #ffffff4f"}} key={_msg.key}>
+                    <div style={{display:"flex",marginTop:"5px"}}>
+                      <img src={_msg.val.img} style={{height:"30px",width:"30px",margin:"0px 5px",borderRadius:"50%"}} alt="error loading.."/>
+                      <div style={{fontSize:"0.8rem",textAlign:"left",flexGrow:1}}>
+                        <div style={{opacity:0.6,marginLeft:"20px",marginTop:"2px",display:"flex"}}>
+                          <div style={{flexGrow:1}}>{user.uid===_msg.val.uid?"You":_msg.val.name}</div>
+                          <div>{processtime(_msg.val.time)}</div>
+                        </div>
+                        <div style={{marginLeft:"20px",marginBottom:"10px"}}>{_msg.val.msg}</div>
+                      </div>
+                    </div>
+                  </div>
+                )
+            })
+            }
+          </div>
+          <div style={{margin:"10px"}}>
+            <div style={{display:"flex"}}>
+              <input type="text" placeholder="Type Someting ..." onKeyUp={checkmsg} id="sendmsg"/>
+              <img src={sendimg} style={{height:"40px",width:"40px",marginRight:"20px",cursor:"pointer"}} onClick={send} alt="error loading.."/>
+            </div>
+            <div className="error" style={{color:"#f00"}}>{senderr}</div>
+          </div>
+      </div>
+    </>
   )
 }
 function GameView({user,back}){
@@ -146,6 +289,7 @@ function GameView({user,back}){
               <GameOver  database={database} data={data} players={players} room={room} playerkey={playerkey} deleteroom={DeleteRoom} newgame={NewGame}/>
               :""
             }
+            <Chat user={user}  room={room} _public={false}/>
           </div>
         </div>
       } 
